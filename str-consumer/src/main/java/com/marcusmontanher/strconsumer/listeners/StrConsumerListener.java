6aca40b9dd2028bfebc648b57c7579de581fe309
Marcus Montanher <marcusmontanher@Air-de-Marcus.bbrouter>
2024-07-11 10:48:27 -0300

    custom listener

--- str-consumer/src/main/java/com/marcusmontanher/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/marcusmontanher/strconsumer/listeners/StrConsumerListener.java
@@ -1,24 +1,24 @@
 package com.marcusmontanher.strconsumer.listeners;
 
+import com.marcusmontanher.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void listener(String message){
         log.info("CREATE ::: Received Message: {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message){
         log.info("LOG ::: Received Message: {}", message);
     }
 
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     public void history(String message){
         log.info("HISTORY ::: Received Message: {}", message);
     }
