786352aab75a35d21cbf676e42e6af0726fecb8a
Marcus Montanher <marcusmontanher@Air-de-Marcus.bbrouter>
2024-07-11 11:02:23 -0300

    record interceptor

--- str-consumer/src/main/java/com/marcusmontanher/strconsumer/config/StringConsumerConfig.java
+++ str-consumer/src/main/java/com/marcusmontanher/strconsumer/config/StringConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.marcusmontanher.strconsumer.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -9,11 +10,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.RecordInterceptor;
 
 import java.util.HashMap;
 
 @RequiredArgsConstructor
 @Configuration
+@Log4j2
 public class StringConsumerConfig {
 
     private final KafkaProperties properties;
@@ -28,10 +31,32 @@ public class StringConsumerConfig {
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> strContainerFactory(ConsumerFactory<String, String> consumerFactory) {
+    public ConcurrentKafkaListenerContainerFactory<String, String> strContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
         var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
         factory.setConsumerFactory(consumerFactory);
         return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> validMessageContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setRecordInterceptor(validMessage());
+        return factory;
+    }
+
+    private RecordInterceptor<String, String> validMessage() {
+        return record -> {
+            if (record.value().contains("Teste")) {
+                log.info("Possui a palavra Teste");
+                return record;
+            }
+            return record;
+        };
     }
 
 }
--- str-consumer/src/main/java/com/marcusmontanher/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/marcusmontanher/strconsumer/listeners/StrConsumerListener.java
@@ -2,23 +2,24 @@ package com.marcusmontanher.strconsumer.listeners;
 
 import com.marcusmontanher.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @StrConsumerCustomListener(groupId = "group-1")
+    @StrConsumerCustomListener(groupId = "group-0")
     public void listener(String message){
         log.info("CREATE ::: Received Message: {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-1")
+    @StrConsumerCustomListener(groupId = "group-0")
     public void log(String message){
         log.info("LOG ::: Received Message: {}", message);
     }
 
-    @StrConsumerCustomListener(groupId = "group-2")
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "validMessageContainerFactory")
     public void history(String message){
         log.info("HISTORY ::: Received Message: {}", message);
     }
