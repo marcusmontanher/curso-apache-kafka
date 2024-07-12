debb91e9118cdea8bb1fcdf09293faa8efa8af43
Marcus Montanher <marcusmontanher@Air-de-Marcus.bbrouter>
2024-07-12 14:48:13 -0300

    alteracoes

--- str-producer/src/main/java/com/marcusmontanher/strproducer/services/StringProducerServices.java
+++ str-producer/src/main/java/com/marcusmontanher/strproducer/services/StringProducerServices.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.View;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -14,7 +13,9 @@ public class StringProducerServices {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message).addCallback(
+        log.info("Sending message: {}", message);
+        kafkaTemplate.send("str-topic", message)
+                .addCallback(
                 success -> {
                     if (success != null) {
                         log.info("Send message with success: {}", message);
